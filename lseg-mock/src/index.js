5ddfe7ce17aff577e001f57510f408d9de0bd746
Ellie Lissner <ellie@neolicense.com>
2022-10-31 16:23:14 -0500

    added bootstrap

--- lseg-mock/src/index.js
+++ lseg-mock/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import ReactDOM from "react-dom/client";
